1cfc33f4b4abaae05be8533b3fc9d7b10c88db49
vijayahasini2007 <vijayahasinim2007@gmail.com>
2026-09-17 21:09:17 +0530

    Created destination data model

---
+++ lib/models/destination.dart
@@ -0,0 +1,13 @@
+class Destination {
+  final String name;
+  final String description;
+  final double averageCost;
+  final List<String> popularPlaces;
+
+  Destination({
+    required this.name,
+    required this.description,
+    required this.averageCost,
+    required this.popularPlaces,
+  });
+}
